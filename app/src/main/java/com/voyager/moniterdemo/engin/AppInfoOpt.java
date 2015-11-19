774d881faea1a190a90fc4d61e9452d341b95461
wuhaojie <w19961009@126.com>
2015-11-19 13:40:48 +0800

    complete get sms module & start get appInfo module.

---
+++ app/src/main/java/com/voyager/moniterdemo/engin/AppInfoOpt.java
@@ -0,0 +1,14 @@
+package com.voyager.moniterdemo.engin;
+
+import com.voyager.moniterdemo.interfaces.Operator;
+
+/**
+ * 获取程序使用情况类
+ * Created by wuhaojie on 2015/11/9.
+ */
+public class AppInfoOpt implements Operator {
+    @Override
+    public void start() {
+
+    }
+}
